6b3fce4a240d2ef3c5036ca9abb99f3bf9417c6e
Student 6 <Student 6@10.8.33.105>
2016-11-16 12:20:51 -0500

    adsfsda

--- Spooks/src/packageEdwinAndAlex/EventEdwinAndAlex.java
+++ Spooks/src/packageEdwinAndAlex/EventEdwinAndAlex.java
@@ -5,6 +5,24 @@ public class EventEdwinAndAlex {
 	public boolean [][] board = new boolean [5][5];
 	public String[][] boardImage;
 	
+	public static final String[]INTRO = {
+			"You enter a room with flickering lights.",
+			"Cautiously, you walk towards the center.",
+			"----------------BANG!-------------------",
+			"The doors have been slammed shut.",
+			"------------*Cue Saw Theme*------------",
+			"'I want to play a game with you.'",
+			"Before you are a series of lights, some turned off and some turned on.",
+			"The rules are simple: enter the coordinates of the switch you want to click.",
+			"The light at that coordinate and the adjacent lights will invert.",
+			"You must turn off all the lights or else the consequences....",
+			"will be severe.",
+			"You have forty turns to complete your assignment.",
+			"Make your choice."
+			
+	};
+	
+	
 	public EventEdwinAndAlex() {
 		// TODO Auto-generated constructor stub
 	}
