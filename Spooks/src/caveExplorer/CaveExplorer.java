31f0ed51bad16ff070457d2a5a484a51dc5d88e7
Student6 <Student6@10.8.33.107>
2016-11-17 12:17:35 -0500

    updated main map

--- Spooks/src/caveExplorer/CaveExplorer.java
+++ Spooks/src/caveExplorer/CaveExplorer.java
@@ -14,7 +14,7 @@ public class CaveExplorer {
 	
 	public static void main(String[] args) {
 		in = new Scanner(System.in);
-		caves = new CaveRoom[5][5];
+		caves = new CaveRoom[5][8];
 		for (int row = 0; row < caves.length; row++) {
 			for (int col = 0; col < caves[row].length; col++) {
 				caves[row][col] = new CaveRoom("This cave has coordinates (" + row +", " + col + ").");
