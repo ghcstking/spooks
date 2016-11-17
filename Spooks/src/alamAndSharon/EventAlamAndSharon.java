4b6f43f2ffa1f5d05d30a4771263247831cb102a
Sharon <Sharon@DESKTOP-1PNI6LC>
2016-11-16 19:57:54 -0500

    basics

--- Spooks/src/alamAndSharon/EventAlamAndSharon.java
+++ Spooks/src/alamAndSharon/EventAlamAndSharon.java
@@ -1,10 +1,33 @@
 package alamAndSharon;
 
+import caveExplorer.CaveExplorer;
+
 public class EventAlamAndSharon {
 
-	public EventAlamAndSharon() {
-		
+	public boolean[][] maze = new boolean[10][10];
+	public static final String[] Begin = { "--Enter dramatic scene--" };
+
+	public void play() {
+		readSequence(Begin);
+		CaveExplorer.print("Are you ready?");
+		while (CaveExplorer.in.nextLine().indexOf("yes") < 0) {
+			CaveExplorer.print("I mean... you have to say yes. Or die here.");
+		}
+		CaveExplorer.print("You have entered the game.");
+	}
+
+	public static void readSequence(String[] seq) {
+		for (int i = 0; i < seq.length; i++) {
+			CaveExplorer.print(seq[i]);
+			CaveExplorer.print(" - - - Press enter - - - ");
+			CaveExplorer.in.nextLine();
+		}
 	}
 
 }
-//push package.
+
+
+
+//outline of event
+
+
