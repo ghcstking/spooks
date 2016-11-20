622147d2568dc37fb32590b11f31bbcbdf0287f8
Sharon <Sharon@DESKTOP-1PNI6LC>
2016-11-20 16:00:37 -0500

    took things out

--- Spooks/src/alamAndSharon/EventAlamAndSharon.java
+++ Spooks/src/alamAndSharon/EventAlamAndSharon.java
@@ -33,26 +33,12 @@ public class EventAlamAndSharon implements Event {
 		CaveExplorer.print(" - - - Press enter - - - ");
 		CaveExplorer.in.nextLine();
 		CaveExplorer.print("SURIVE!");
-		/*
-		arr2D = new String[5][5];
-		pic = new String[5][5];
-		for(int row = 0; row < arr2D.length; row++){
-			for(int col = 0; col < arr2D[row].length; col++){
-				arr2D[row][col] = "(" + row + "," + col + ")";
-			}
-			
-		}
-		//HOMEWORK FUNCTION
-		printGrid(pic);
-		// start position
-		i = 2;
-		j = 3;
-		in = new Scanner(System.in);
-		startExploring();
-		*/
+		
 	}
 	// ********* In this class, we will only generate the map. *********
 	//******The input class will take in the user input to move the person and update the map.********
+	
+
 	
 /*
 	private static void printGrid(String[][] picArray) {
@@ -76,54 +62,6 @@ public class EventAlamAndSharon implements Event {
 		
 	}
 
-	private static void startExploring() {
-		// TODO Auto-generated method stub
-		while(true){
-			//printPic(pic);
-			System.out.println("You are in room " + arr2D[i][j]);
-			System.out.println("What do you want to do?");
-			String input = in.nextLine();
-			while(!isValid(input)){
-				System.out.println("Please enter W, A, S, or D.");
-				input = in.nextLine();
-			}
-			interpretInput(input.toLowerCase());
-			
-		}
-	}
-
-		private static void interpretInput(String input) {
-			int iOrig = i;
-			int jOrig = j;
-			if(input.equals("w") && i > 0) i--;
-			else if(input.equals("a") && j > 0) j--;
-			else if(input.equals("s") && i < arr2D.length-1) i++;
-			else if(input.equals("d") && j < arr2D[i].length-1) j++;
-			
-			if(iOrig == i && jOrig == j){
-				System.out.println("There is a wall in your path");
-			}
-	}
-
-		private static boolean isValid(String input) {
-			String lc = input.toLowerCase().trim();
-			String[] keys = {"w","a","s","d"};
-			for(String key:keys){
-				if(key.equals(lc))return true;
-			}
-			return false;
-	}
-
-		
-		private static void mines() {
-			// Lesson 2 on the mines
-			boolean [][] mines = new boolean[100][100];
-			createMines(mines, 5000);
-			String[][] field =	new String[mines.length][mines[0].length];
-			matchValue(field,mines);	
-			printPic(field);	
-		}
-	
 		public static void printPic(String[][] pic){
 			for(int y = 0; y < pic.length; y++){
 				for(int x = 0; x < pic[y].length; x++){
@@ -150,7 +88,9 @@ public class EventAlamAndSharon implements Event {
 				}
 			}
 		}
-	
+	//we can use something like this countAdacent func to warn player of nearby ghost
+	 //and also to check if the player can move in that direction.
+	  
 		private static String countAdjacent(boolean[][] mines, int r, int c) {
 			
 			int count = 0;
@@ -163,37 +103,8 @@ public class EventAlamAndSharon implements Event {
 			return count + "";
 		}
 	
-		private static int vaildAndTrue(boolean[][] mines, int i, int j) {
-			
-			
-			if(i>0 && i <mines.length && j>=0 && j <mines[1].length && mines[i][j]){
-				return 1;
-			}
-			return 0;
-		}
 	
-		private static void createMines(boolean[][] mines, int numberOfMines) {
-			
-			while(numberOfMines > 0){
-				int row = (int)(Math.random()*mines.length);
-				int col = (int)(Math.random()*mines[0].length);
-				if(!mines[row][col]){
-					mines[row][col]= true;
-					numberOfMines --;
-				}
-			}	
-		}
-		
-
-	public static void readSequence(String[] seq) {
-		for (int i = 0; i < seq.length; i++) {
-			CaveExplorer.print(seq[i]);
-			CaveExplorer.print(" - - - Press enter - - - ");
-			CaveExplorer.in.nextLine();
-		}
-	}
 */
-	
 	
 	public static void readSequence(String[] seq) {
 		for (int i = 0; i < seq.length; i++) {
