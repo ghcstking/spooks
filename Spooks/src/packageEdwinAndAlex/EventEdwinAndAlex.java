5e58a09e542c2e69b51ba8b0e2b2cb0f2781abbb
Edwin <Edwin@Lenovo-PCx.home>
2016-11-20 19:45:56 -0500

    more stuff

--- Spooks/src/packageEdwinAndAlex/EventEdwinAndAlex.java
+++ Spooks/src/packageEdwinAndAlex/EventEdwinAndAlex.java
@@ -33,8 +33,7 @@ public class EventEdwinAndAlex implements Event {
 		CaveExplorer.print("Let the games begin.");
 		CaveExplorer.inventory.setMap(false);
 		CaveExplorer.startExploring(false);
-		generateBoard(board);
-		printBoard(board);
+		AlexFieldGenerator.startGame(board);
 		//TODO FIX THIS SHIT
 	}
 
@@ -45,53 +44,8 @@ public class EventEdwinAndAlex implements Event {
 			CaveExplorer.in.nextLine();
 		}
 	}
-	public static void generateBoard(boolean [][] board){
-		for(int i = 0; i < 11; i ++){
-			int randRow = (int)Math.random()*board.length;
-			int randCol = (int)Math.random()*board[0].length;
-			
-			//while random light is turned off (true is off)
-			while(board[randRow][randCol]){
-				randRow = (int)Math.random()*board.length;
-				randCol = (int)Math.random()*board[0].length;
-			}
-			board[randRow][randCol] = !board[randRow][randCol];
-		}
-	//testing	dasda
-	}
-	public void printBoard(boolean [][] board){
-		String boardImage = " ";
-		for(int i = 0; i < board[0].length-1;i++){
-			boardImage+="____";
-		}
-		boardImage+="___\n";
-		for(boolean[] row : board ){
-			for(int i = 0; i < 3; i++){
-				String text = "";
-				for(boolean light: row){
-					text+="|";
-					if(i == 0){
-						text+="   ";
-					}
-					else if (i == 1){
-						if(light){
-							text+="+";
-						}
-						else{
-							text+="-";
-						}
-
-					}
-					else if (i == 2){
-						text+= "___";
-					}
-				}
-				text+="|";
-				boardImage+=text+"\n";
-			}
-		}
-		CaveExplorer.print(boardImage);
-	}
+	
+	
 
 
 	
