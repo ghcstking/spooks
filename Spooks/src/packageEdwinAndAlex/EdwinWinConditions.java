52ec69e8f48edc5381a60dfa3eabe244c1a79250
Student 6 <Student 6@10.8.33.105>
2016-11-22 12:29:10 -0500

    WUBBALUBBADUBDUBBBBBBBBBBBBBBBB

--- Spooks/src/packageEdwinAndAlex/EdwinWinConditions.java
+++ Spooks/src/packageEdwinAndAlex/EdwinWinConditions.java
@@ -19,89 +19,8 @@ public class EdwinWinConditions {
 		return true;
 	}
 
-	public static void changeBoard(String rowCord,String colCord){
-//		if(cheatCodeEntered(rowCord)||(cheatCodeEntered(colCord))){
-//			
-//		}
-			int r = Integer.parseInt(rowCord);
-			int c = Integer.parseInt(colCord);
-			while(!isValidPoint(r,c)){
-				CaveExplorer.print("Are you braindead? There's no lightbulb at that point.");
-				CaveExplorer.print("Enter row coordinate.");
-				r = Integer.parseInt(CaveExplorer.in.nextLine());
-				CaveExplorer.print("Enter column coordinate.");
-				c = Integer.parseInt(CaveExplorer.in.nextLine());
-			}
-			changeLights(r,c);
-		}
 
-	private static void changeLights(int r, int c) {
-		// TODO Auto-generated method stub
-		//have to first establish special cases
-		AlexFieldGenerator.board[r][c] = !AlexFieldGenerator.board[r][c];
-		if(c==0){
-			AlexFieldGenerator.board[r][c+1] = !AlexFieldGenerator.board[r][c+1];
-			if(r==0){
-				AlexFieldGenerator.board[r+1][c] = !AlexFieldGenerator.board[r+1][c];
-			}
-			else if(r==AlexFieldGenerator.board.length-1){
-				AlexFieldGenerator.board[r-1][c] = !AlexFieldGenerator.board[r-1][c];
-			}
-			else{
-				AlexFieldGenerator.board[r-1][c] = !AlexFieldGenerator.board[r-1][c];
-				AlexFieldGenerator.board[r+1][c] = !AlexFieldGenerator.board[r+1][c];
-			}
-		}
-		else if(c==AlexFieldGenerator.board[0].length-1){
-			AlexFieldGenerator.board[r][c-1] = !AlexFieldGenerator.board[r][c-1];
-			if(r==0){
-				AlexFieldGenerator.board[r+1][c] = !AlexFieldGenerator.board[r+1][c];
-			}
-			else if (r==AlexFieldGenerator.board.length-1){
-				AlexFieldGenerator.board[r-1][c] = !AlexFieldGenerator.board[r-1][c];
-			}
-			else{
-				AlexFieldGenerator.board[r-1][c] = !AlexFieldGenerator.board[r-1][c];
-				AlexFieldGenerator.board[r+1][c] = !AlexFieldGenerator.board[r+1][c];
-			}
-		}
-		else if (r==0){
-			AlexFieldGenerator.board[r+1][c] = !AlexFieldGenerator.board[r+1][c];
-			if(c==0){
-				AlexFieldGenerator.board[r][c+1] = !AlexFieldGenerator.board[r][c+1];
-			}
-			else if (c==AlexFieldGenerator.board[0].length-1){
-				AlexFieldGenerator.board[r][c-1] = !AlexFieldGenerator.board[r][c-1];
-			}
-			else{
-				AlexFieldGenerator.board[r][c+1] = !AlexFieldGenerator.board[r][c+1];
-				AlexFieldGenerator.board[r][c-1] = !AlexFieldGenerator.board[r][c-1];
-			}
-		}
-		else if (r==AlexFieldGenerator.board.length-1){
-			AlexFieldGenerator.board[r-1][c] = !AlexFieldGenerator.board[r-1][c];
-			if(c==0){
-				AlexFieldGenerator.board[r][c+1] = !AlexFieldGenerator.board[r][c+1];
-			}
-			else if (c==AlexFieldGenerator.board[0].length-1){
-				AlexFieldGenerator.board[r][c-1] = !AlexFieldGenerator.board[r][c-1];
-			}
-			else{
-				AlexFieldGenerator.board[r][c+1] = !AlexFieldGenerator.board[r][c+1];
-				AlexFieldGenerator.board[r][c-1] = !AlexFieldGenerator.board[r][c-1];
-			}
-		}
-		else{
-			AlexFieldGenerator.board[r-1][c] = !AlexFieldGenerator.board[r-1][c];
-			AlexFieldGenerator.board[r+1][c] = !AlexFieldGenerator.board[r+1][c];
-			AlexFieldGenerator.board[r][c+1] = !AlexFieldGenerator.board[r][c+1];
-			AlexFieldGenerator.board[r][c-1] = !AlexFieldGenerator.board[r][c-1];
-			
-		}
-
-	}
-
-	private static boolean isValidPoint(int rowCord, int colCord){
+	public static boolean isValidPoint(int rowCord, int colCord){
 		boolean validRow = false;
 		boolean validCol = false;
 		for(int r = 0; r < AlexFieldGenerator.board.length-1;r++){
@@ -113,7 +32,7 @@ public class EdwinWinConditions {
 		return (validRow&&validCol);
 	}
 
-	private static boolean cheatCodeEntered(String input){
+	public static boolean cheatCodeEntered(String input){
 		return(input.equals(AlexFieldGenerator.cheatCode));
 	}
 }
