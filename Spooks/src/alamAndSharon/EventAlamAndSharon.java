3d4aafe05c79797ae74c6dbe572ecbbbd803b7e9
Sharon <Sharon@DESKTOP-1PNI6LC>
2016-11-22 08:32:34 -0500

    forgoten flvr text

--- Spooks/src/alamAndSharon/EventAlamAndSharon.java
+++ Spooks/src/alamAndSharon/EventAlamAndSharon.java
@@ -10,7 +10,10 @@ import caveExplorer.Event;
 public class EventAlamAndSharon implements Event {
 
 	public boolean[][] maze = new boolean[10][10];
-	public static final String[] Begin = { "--Enter dramatic scene--" };
+	public static final String[] Begin = { "Unfortunately for you, You have entered a pitch black room.",
+			"It's too late to turn back now.","You must pass this test to prove you're worthy of moving to the next round.",
+			"Navigate this this room with your moral compass." ,"You will be given a map when you are within proximity of a ghost",
+			"May the forces be with you."};
 	public static final String[] Win = {"Horrayyy!!You have escaped alive!!"};
 	public static final String[] Lost = {"Game Over. You've been found."};
 	public static Scanner in;
